279a97445a9e30c5c933479588dc913be9a05b43
OlgaSvitelska <olgasvitelska@gmail.com>
2014-04-14 21:06:18 -0700

    find

---
+++ find_a_job_matched_role_profile_view.php
@@ -0,0 +1,362 @@
+<div class="container">
+	<div class="box_profile row">
+		<h4>Jobs Matched Detail Page</h4>
+	</div>
+	
+	<div class="row uppercase" style="padding: 0 0 0 10% !important;">
+		<div class="">
+			<label class="col-sm-6 control-label" ><h4> Role:VP-Business Strategy</h4>
+			</label>
+		</div>
+		<div>
+			<ul class="pagination col-sm-6">
+			  <li class=""><a href="#">&lt;&nbsp;previous job</a></li>
+			  <li class="bord_lr"><a href="#">listed jobs</a></li>
+			  <li class="active"><a href="#">next job&nbsp;&gt; <span class="sr-only">(current)</span></a></li>
+		</ul>
+		
+		</div>
+	</div>
+
+	<div  style="margin-left:10%;">
+		<img class="" src="images/line.jpg" alt="">
+	</div>
+	
+	<div class="marleft_15">
+		<p class=" job_det uppercase"> Role Matches</p>
+		<div class="row">
+			<div class="col-sm-3 role_box" >
+				<p class="mb_8">Industry</p>
+			</div>
+			
+			<div class="col-sm-8 pull-left check-box_margin">
+				<div class="checkbox">
+					<label style="font-weight:bold;">
+					<input type="checkbox" value="">
+					Yes
+					</label>
+				</div>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-sm-3 role_box" >
+				<p class="mb_8">Role Level</p>
+			</div>
+			
+			<div class="col-sm-8 pull-left check-box_margin">
+				<div class="checkbox">
+					<label style="font-weight:bold;">
+					<input type="checkbox" value="">
+					Yes
+					</label>
+				</div>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-sm-3 role_box">
+				<p class="mb_8">Years of experience</p>
+			</div>
+			
+			<div class="col-sm-8 pull-left check-box_margin">
+				<div class="checkbox">
+					<label style="font-weight:bold;">
+					<input type="checkbox" value="">
+					Yes
+					</label>
+				</div>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-sm-3 role_box">
+				<p class="mb_8">Function</p>
+			</div>
+			
+			<div class="col-sm-8 pull-left check-box_margin">
+				<div class="checkbox">
+					<label style="font-weight:bold;">
+					<input type="checkbox" value="">
+					Yes
+					</label>
+				</div>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-sm-3 role_box">
+				<p class="mb_8">Salary</p>
+			</div>
+			
+			<div class="col-sm-8 pull-left check-box_margin">
+				<div class="checkbox">
+					<label style="font-weight:bold;">
+					<input type="checkbox" value="">
+					Yes
+					</label>
+				</div>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-sm-3 role_box">
+				<p class="mb_8">Country</p>
+			</div>
+			
+			<div class="col-sm-8 pull-left check-box_margin">
+				<div class="checkbox">
+					<label style="font-weight:bold;">
+					<input type="checkbox" value="">
+					Yes
+					</label>
+				</div>
+			</div>
+		</div>
+					
+	</div>
+						
+						
+				
+						
+			
+			<div class="hr_grey"><hr/></div>
+			
+			<div class="marleft_15">
+		<p class=" job_det uppercase">The company</p>
+		<div class="row">
+			<div class="col-sm-3 role_box">
+				<p class="mb_8">Company name</p>
+			</div>
+			
+			<div class="col-sm-8 pull-left role-margin">
+				<p class="form-control-static" style="margin-left:-10%;">ABC Company PLC</p>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-sm-3 role_box">
+				<p class="mb_8">Location</p>
+			</div>
+			
+			<div class="col-sm-8 pull-left role-margin">
+				<p class="form-control-static" style="margin-left:-10%;">Pune, India</p>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-sm-3 role_box">
+				<p class="mb_8">Number of Employees</p>
+			</div>
+			
+			<div class="col-sm-8 pull-left role-margin">
+						<p class="form-control-static" style="margin-left:-10%;">500-1,500</p>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-sm-3 role_box">
+				<p class="mb_8">Website Address</p>
+			</div>
+			
+			<div class="col-sm-8 pull-left role-margin blue">
+				<p class="form-control-static" style="margin-left:-10%;">www.abccompany.com</p>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-sm-3 role_box">
+				<p class="mb_8">About the company</p>
+			</div>
+			
+			<div class="col-sm-7 pull-left" style="margin:0 0 0 1%;">
+						<p class="form-control-static" style="margin-left:-10%;">ABC company is a web based lender focused on small and medium businesses in the United States,backed by WWP Parteneres. We provide loans of between $2,000 and 50,000 to hight quality...read more </p>
+			</div>
+		</div>
+		</div>
+		
+		<div class="hr_grey"><hr/></div>
+			
+			<div class="marleft_15">
+		<p class=" job_det uppercase">The Role</p>
+		<div class="row">
+			<div class="col-sm-3 role_box">
+				<p class="mb_8">Industry</p>
+			</div>
+			
+			<div class="col-sm-8 pull-left role-margin">
+						<p class="form-control-static" style="margin-left:-10%;">Internet</p>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-sm-3 role_box">
+				<p class="mb_8">Level of Role</p>
+			</div>
+			
+			<div class="col-sm-8 pull-left role-margin">
+				<p class="form-control-static" style="margin-left:-10%;">Senior Executive</p>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-sm-3 role_box">
+				<p class="mb_8">Title of Role</p>
+			</div>
+			
+			<div class="col-sm-8 pull-left role-margin">
+						<p class="form-control-static" style="margin-left:-10%;">VP, Business strategy</p>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-sm-3 role_box">
+				<p class="mb_8">Desired Years of Experience</p>
+			</div>
+			
+			<div class="col-sm-8 pull-left role-margin">
+				<p class="form-control-static" style="margin-left:-10%;">15 years</p>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-sm-3 role_box">
+				<p class="mb_8">Function</p>
+			</div>
+			
+			<div class="col-sm-8 pull-left role-margin">
+						<p class="form-control-static" style="margin-left:-10%;">Strategy</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-3 role_box">
+				<p class="mb_8">Target Compensation</p>
+			</div>
+			
+			<div class="col-sm-8 pull-left role-margin">
+						<p class="form-control-static" style="margin-left:-10%;">US$100,000</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-3 role_box">
+				<p class="mb_8">Location of role</p>
+			</div>
+			
+			<div class="col-sm-8 pull-left role-margin">
+						<p class="form-control-static" style="margin-left:-10%;">Pune, India</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-3 role_box">
+				<p class="mb_8">Expected Start Date </p>
+			</div>
+			
+			<div class="col-sm-8 pull-left role-margin">
+						<p class="form-control-static" style="margin-left:-10%;">March 2014</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4 role_box">
+				<p class="mb_8">Description of Role/Experiences Desired</p>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-sm-3 role_box">
+				<p class="mb_8">Expected Start Date </p>
+			</div>
+			
+			<div class="col-sm-8 pull-left role-margin">
+						<p class="form-control-static" style="margin-left:-10%;">March 2014</p>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-sm-12">
+				<p class="mb_8  role_box">Job Description </p>
+			</div>
+			
+			<div class="col-sm-8 pull-left role-margin">
+						<p class="form-control-static" style="margin:0 0 10px 3%;">The VP of Strategy will report to one of the company's Co-Founders and will be primerily responsible for originating and developing the companies most critical relationships with marketing partners, government agencies, the press, and other key constituencies. Aditionally, the role will be involved in formulating and executing the companie's external and marketing strategy, and serving as a stand-in for the founders in a variety of external relationships. </p>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-sm-12 role_box">
+				<p class="mb_8">Key Function will include: </p>
+			</div>
+			
+			<div class="col-sm-12 pull-left" style="margin-left:1%;">
+						<ul >
+						<li> Marketing Partnership: source and mine key sources of borrowers,including online affiliates, local economic development groups, and other financial institutions;</li>
+						<li> Government Affairs:working with our senior team and legal advisers to formulate and execute our regulatory strategy; keep tabs on relevant legal developments and ensure that the company is well positioned in the face of constantly shifting regulatory landscape;</li>
+						<li> Communications and PR: work with our senior management team and outside PR team to formulate the companies public posiyioning strategy; help manage critical press relationships and build awareness of the business;</li>
+						<li>Strategy and Company Development: serve as a senior member of our management team, contributing to major strategic direction and company management efforts.</li>
+						</ul>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-sm-12 role_box">
+				<p class="mb_8">This role will take a leadership role in bringing our business to the next level.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-12 role_box">
+				<p class="mb_8">Desired Skills and Experience</p>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-sm-12 role_box">
+				<p class="mb_8">Qualifications: Ideal candidates will have many of the following characteristics:</p>
+			</div>
+			
+			<div class="col-sm-12 pull-left">
+						<ul style="margin-left:1%;">
+							<li>5+ years of experience with strong record of exceeding goals</li>
+							<li>Demonstradable track record in developing senior relationships and using those to drive business value</li>
+							<li>Experience in marketing and communication</li>
+							<li>Interest in and understanding of financial services</li>
+							<li>Strong interest in and aptitude for public affairs</li>
+							<li>Experience with small business, comercial lending, and/or franchises a plus</li>
+							<li>Comfort with regular travel(New York, DC particularly)</li>
+							<li>Enthusiazm for a fast paced, team oriented, start-up with sometimes long hours</li>
+							<li>Identification with missuon: "to emprover small business through innovation in finance and tech."</li>
+							<li>Alignment with our core values: Respect, Peoplle are important,Individual Empowerment, Desire to Improve/High expectations, Do What's Right, and Service</li>
+						</ul>
+			</div>
+		</div>
+		
+	</div>
+	
+	<div class="hr_grey"><hr/></div>
+	<div class="marleft_15">
+		<div class="row">
+			<div class="col-sm-3 role_box">
+				<p class="mb_8">Contact</p>
+			</div>
+			
+			<div class="col-sm-8 pull-left role-margin">
+				<p class="form-control-static" style="margin-left:-10%;">T.H. Osun</p>
+				<p class="form-control-static" style="margin-left:-10%;">+46 803 456 7777</p>
+				<p class="form-control-static" style="margin-left:-10%;">thosun@abccompany.com</p>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-sm-3 role_box">
+				<p class="mb_8">Apply Here</p>
+			</div>
+			
+			<div class="col-sm-8 pull-left role-margin blue">
+						<p class="form-control-static" style="margin-left:-10%;">www.hireonline.com</p>
+			</div>
+		</div>
+	</div>
+	
+			
+			
+	
+						
+</div>
